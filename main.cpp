311e7b1d4f30a6d8386a5209e1f0f3cfc1dbd780
david cramer <dontcallmebrownie@gmail.com>
2024-09-02 13:57:16 -0500

    running through the basics pt. BPM display

--- main.cpp
+++ main.cpp
@@ -5,44 +5,98 @@
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 800;
 
+bool init();
+bool load();
+void close();
+
+SDL_Window* win = NULL;
+SDL_Surface* scr = NULL;
+SDL_Surface* hello = NULL;
 
 
-int main( int argc, char* argv[] ) {
 
-    SDL_Window* win = NULL;
-    SDL_Surface* scr = NULL;
+bool init() {
 
+    bool success = true;
 
     if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
+
         printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+
+        success = false;
     }
     else {
         std::cout << "SDL init success!\n";
 
         win = SDL_CreateWindow ( "Hello World!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+
         if ( win == NULL ) {
                 printf("Window init FAILED Error: %s\n", SDL_GetError() );
+
+                success = false;
         }
         else {
+
             std::cout << "Window init success!\n";
 
             scr = SDL_GetWindowSurface( win );
+        }
+    }
 
-            SDL_FillRect( scr, NULL, SDL_MapRGB( scr->format, 0xFF, 0xFF, 0xFF ) );
+return success;
+}
 
-            SDL_UpdateWindowSurface( win );
+bool load() {
 
+    bool success = true;
 
+    hello = SDL_LoadBMP( "./assets/hello.bmp" );
 
+    if( hello == NULL ) {
+
+        printf( "Load BMP Failed! Error: %s\n", SDL_GetError() );
+        success = false;
+    }
+
+return success;
+}
+
+void close() {
+
+    SDL_FreeSurface( hello );
+    hello = NULL;
+
+    SDL_DestroyWindow( win );
+    win = NULL;
+
+    printf("Closing...\n");
+
+    SDL_Quit();
+
+}
+
+int main( int argc, char* argv[] ) {
+
+    if( !init() ) {
+
+        printf("Failed to init SDL! \n");
+    }
+    else {
+        if( !load() ) {
+
+            printf("Failed to Load img!\n");
+        }
+        else {
+
+            SDL_BlitSurface( hello, NULL, scr, NULL);
+
+            SDL_UpdateWindowSurface(win);
 
             SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
         }
     }
 
-     SDL_DestroyWindow( win );
-     SDL_Quit();
-
-
+    close();
 
 return 0;
 }
